21124f4c8c1ed0af65aaf848763e26a140ee944e
Anastasia Maisuradze <90347089+Astasmaisuradze@users.noreply.github.com>
2023-07-07 19:42:10 +0400

    clean code, rearrange variables

--- src/test/java/LogInTests.java
+++ src/test/java/LogInTests.java
@@ -12,7 +12,6 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import LogInPageObjects.InValidUserPage;
-
 import java.time.Duration;
 
 public class LogInTests {
