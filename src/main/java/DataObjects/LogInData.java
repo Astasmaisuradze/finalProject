8f8f21be4d799e292080f258eec876e44b84eaa4
Anastasia Maisuradze <90347089+Astasmaisuradze@users.noreply.github.com>
2023-07-07 10:25:44 +0400

    first addition

---
+++ src/main/java/DataObjects/LogInData.java
@@ -0,0 +1,7 @@
+package DataObjects;
+
+public interface LogInData {
+    String
+    username = "standard_user",
+    password = "secret_sauce";
+}
---
+++ src/main/java/PageObjects/LogInPage.java
@@ -0,0 +1,40 @@
+package PageObjects;
+
+import DataObjects.LogInData;
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import static DataObjects.LogInData.username;
+import static DataObjects.LogInData.password;
+
+
+public class LogInPage {
+    WebDriver driver;
+
+    public LogInPage (WebDriver driver){
+        this.driver = driver;
+    }
+
+    By usernameInput = By.id("user-name");
+    By passwordInput = By.id("password");
+    By loginButton = By.id("login-button");
+
+    @Step("Username Input:{0}")
+    public LogInPage UsernameInput (String username){
+        driver.findElement(usernameInput).sendKeys(LogInData.username);
+        return this;
+    }
+    @Step("Password Input:{0}")
+    public LogInPage PasswordInput (String password) {
+        driver.findElement(passwordInput).sendKeys(LogInData.password);
+        return this;
+    }
+    @Step("Click on Login Button")
+    public LogInPage clickOnLogInButton() throws InterruptedException {
+        driver.findElement(loginButton).click();
+        Thread.sleep(5000);
+        return this;
+    }
+}
+
+
